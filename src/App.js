2a0ae377b255a6fe3d7be85a28f9d97eab571f9f
Akaash <akaashsashiraj30@gmail.com>
2025-03-24 18:47:01 +0530

    movie detail added

--- src/App.js
+++ src/App.js
@@ -5,14 +5,15 @@ import Moviecard from './Moviecard';
 
 const API_URL = 'http://www.omdbapi.com/?apikey=3636da8f';
 
+/* eslint-disable no-unused-vars */
 const movie = {
-  
     "Title": "Italian Spiderman",
     "Year": "2007",
     "imdbID": "tt2705436",
     "Type": "movie",
     "Poster": "https://m.media-amazon.com/images/M/MV5BYWNiMmNlNmQtZTI2MS00MzAxLTgxM2QtNDY3ZGQxNDMwZDgzXkEyXkFqcGc@._V1_SX300.jpg"
 }
+/* eslint-enable no-unused-vars */
 
 const App = () => {
 
